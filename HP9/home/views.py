ad702455ac41adfd4c96b3620e377a8d1042f8dd
minhteky2011@gmail.com <minhteky2011@gmail.com>
2025-03-29 15:38:41 +0700

    add page home

--- HP9/home/views.py
+++ HP9/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, 'home/home.html')
+
+def intro_view(request):
+    return render(request, 'home/introduction.html')
