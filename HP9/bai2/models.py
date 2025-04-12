926feacd11db3562b3a213e8aa9e995be2544777
minhteky2011@gmail.com <minhteky2011@gmail.com>
2025-04-12 15:40:11 +0700

    tao app

--- HP9/bai2/models.py
+++ HP9/bai2/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your views here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    cover_image = models.ImageField(upload_to = 'book_covers/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
